06a2d5fe538f4071e2b37eeb0d2712d43e2c2aa0
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-08-04 12:12:22 +0100

    Add ToDoItem class
    Adds the ToDoItem class, which represents a single to-do item. Each item has a unique identifier (ID), a description of the task, a flag to indicate completion status (IsComplete), and the date and time when the item was created (DateCreated).

--- BlazorToDoList.App/Models/ToDoItem.cs
+++ BlazorToDoList.App/Models/ToDoItem.cs
@@ -1,12 +1,13 @@
-﻿using System;
+using System; 
 
 namespace BlazorToDoList.App.Models
 {
+    // Define the ToDoItem class to represent a single to-do item.
     public class ToDoItem
     {
-        public Guid ID { get; set; }
-        public string Description { get; set; }
-        public bool IsComplete { get; set; }
-        public DateTime DateCreated { get; set; }
+        public Guid ID { get; set; } // A unique identifier for the to-do item.
+        public string Description { get; set; } // The description or task of the to-do item.
+        public bool IsComplete { get; set; } // A flag to indicate whether the task is completed or not.
+        public DateTime DateCreated { get; set; } // The date and time when the to-do item was created.
     }
 }
