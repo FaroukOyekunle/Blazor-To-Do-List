910fa7af9596a7fbd403ac56b3c2ff3d8d50349e
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-08-08 11:34:05 +0100

    Add IToDoService interface for managing ToDoItems
    Added IToDoService interface, which defines the contract for a service responsible for managing ToDoItems. The interface declares methods to get all ToDoItems, get a specific ToDoItem by its ID, add a new ToDoItem, toggle the IsComplete status of a ToDoItem, and delete a ToDoItem with the specified ID. The concrete implementation of this interface will be provided by the ToDoService class.

--- BlazorToDoList.App/Services/IToDoService.cs
+++ BlazorToDoList.App/Services/IToDoService.cs
@@ -1,16 +1,46 @@
-﻿using BlazorToDoList.App.Models;
+using BlazorToDoList.App.Models;
 using System;
 using System.Collections.Generic;
 
 namespace BlazorToDoList.App.Services
 {
+    /// <summary>
+    /// Interface for a service responsible for managing ToDoItems.
+    /// </summary>
     public interface IToDoService
     {
+        /// <summary>
+        /// Get a list of all ToDoItems.
+        /// </summary>
+        /// <returns>A list of ToDoItems.</returns>
         List<ToDoItem> Get();
-        ToDoItem Get(Guid ID);
-        List<ToDoItem> Add(ToDoItem toDoItem);
-        List<ToDoItem> Toggle(Guid id);
-        List<ToDoItem> Delete(Guid ID);
 
+        /// <summary>
+        /// Get a specific ToDoItem by its ID.
+        /// </summary>
+        /// <param name="ID">The ID of the ToDoItem to retrieve.</param>
+        /// <returns>The ToDoItem with the specified ID, or null if not found.</returns>
+        ToDoItem Get(Guid ID);
+
+        /// <summary>
+        /// Add a new ToDoItem to the list of ToDoItems.
+        /// </summary>
+        /// <param name="toDoItem">The ToDoItem to add.</param>
+        /// <returns>The updated list of ToDoItems.</returns>
+        List<ToDoItem> Add(ToDoItem toDoItem);
+
+        /// <summary>
+        /// Toggle the IsComplete status of a ToDoItem.
+        /// </summary>
+        /// <param name="id">The ID of the ToDoItem to toggle.</param>
+        /// <returns>The updated list of ToDoItems.</returns>
+        List<ToDoItem> Toggle(Guid id);
+
+        /// <summary>
+        /// Delete a ToDoItem with the specified ID from the list of ToDoItems.
+        /// </summary>
+        /// <param name="ID">The ID of the ToDoItem to delete.</param>
+        /// <returns>The updated list of ToDoItems.</returns>
+        List<ToDoItem> Delete(Guid ID);
     }
 }
